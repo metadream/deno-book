59a8704d962d60f31939a7965d672b70114f1d90
openflake <app@zerg.cc>
2023-02-28 20:15:10 +0800

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export { dirname, resolve } from "https://deno.land/std@0.177.0/path/mod.ts";
-export { core } from "https://deno.land/x/core@0.0.1/mod.ts";
+export { core } from "https://deno.land/x/core@0.0.2/mod.ts";
 export { Marked } from "https://deno.land/x/markdown@v2.0.0/mod.ts";

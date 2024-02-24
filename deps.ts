1dc5ae6d73e02a2b46b13d2187718ca99214198e
metadream <app@zerg.cc>
2024-02-24 12:25:58 +0800

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * from "https://deno.land/x/spring@0.3.5/mod.ts";
+export * from "https://deno.land/x/spring@0.3.6/mod.ts";
 export { dirname, resolve } from "https://deno.land/std@0.217.0/path/mod.ts";
 export { parse as parseYaml } from "https://deno.land/std@0.217.0/yaml/mod.ts";
 export { marked } from 'https://deno.land/x/marked@1.0.2/mod.ts';
